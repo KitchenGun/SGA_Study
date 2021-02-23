1e5d18b8d23b0e78a3648cee027bc50dbb11c0bb
KitchenGun <kitchengun124@gmail.com>
2021-02-23 12:37:20 +0900

    DirectX연결

--- DirectX11SelfStudy/WindowPopup/stdafx.h
+++ DirectX11SelfStudy/WindowPopup/stdafx.h
@@ -3,3 +3,12 @@
 //À©µµ¿ì
 #include<Windows.h>
 #include<assert.h>
+
+//DirectX
+#include <d3dcompiler.h>
+#include <d3d11.h>
+#include <D3DX10math.h>
+
+#pragma comment(lib,"dxgi.lib")
+#pragma comment(lib,"d3d11.lib")
+#pragma comment(lib,"d3dcompiler.lib")
