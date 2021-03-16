730e45e935a2d3ce5fb2a976fd74168bb0f9f382
KitchenGun <kitchengun124@gmail.com>
2021-03-16 17:19:52 +0900

    리스트와 벡터의 차이 공부 & 리스트 삽입과 삭제 구현

--- DataStructure/LinkedList/Main.cpp
+++ DataStructure/LinkedList/Main.cpp
@@ -38,6 +38,39 @@ void Push(Node** head, Node* node)
 	}
 }
 
+void insertHead(Node** head, Node* node)
+{
+	if (*head == NULL)
+	{
+		*head = node;
+	}
+	else
+	{
+		node->Next = *head;
+		*head = node;
+	}
+}
+
+void Remove(Node** head, Node* remove)
+{
+	if (*head == remove)
+	{
+		*head = remove->Next;
+	}
+	else
+	{
+		Node* current = *head;
+		while (current != NULL && current->Next != remove)
+		{
+			current = current->Next;
+		}
+		if (current->Next != NULL)
+		{
+			current->Next = remove->Next;
+		}
+	}
+}
+
 Node* GetNode(Node* head, int location)//location 몇번째 데이터 찾아올지
 {
 	Node* current = head;
@@ -77,5 +110,31 @@ int main()
 		printf("list[%d] : %d\n", i, current->Data);
 	}
 	printf("\n");
+	Node* temp = GetNode(list, 3);
+	printf("list[%d] : %d\n", 3, temp->Data);
+	printf("\n");
+
+	Node* newNode = Create(-1);
+	insertHead(&list, newNode);
+	count = GetNodeCount(list);
+	for (int i = 0; i < count; i++)
+	{
+		Node* current = GetNode(list, i);
+
+		printf("list[%d] : %d\n", i, current->Data);
+	}
+	printf("\n");
+
+	Remove(&list, temp);
+	count = GetNodeCount(list);
+	for (int i = 0; i < count; i++)
+	{
+		Node* current = GetNode(list, i);
+
+		printf("list[%d] : %d\n", i, current->Data);
+	}
+	printf("\n");
 	return 0;
 }
+//vector 주소가 연속												  (변경이 없는경우 사용하는 것이 유리함 탐색에 용의 하지만 삽입 삭제가 느림)
+//list 노드의 다음노드가 주소를 갖고있음(메모리영역이 연속이 아님)     (삽입 삭제가 빠름 탐색은 느림)
