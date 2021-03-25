e062ea23392d01fdfd9ab1c7b8164935d1bf2e5d
KitchenGun <kitchengun124@gmail.com>
2021-03-25 17:42:39 +0900

    Queue 헤더 작성
    fifo enqueue dequeue

---
+++ DataStructure/Queue/Circular_Queue.h
@@ -0,0 +1,52 @@
+#pragma once
+
+
+template<typename T>class Circular_Queue
+{
+public:
+	Circular_Queue(int capacity) : capacity(capacity)
+	{//실제 배열의 크기는 capacity + 1
+		datas = new T[capacity + 1];
+	}
+
+	~Circular_Queue()
+	{
+		delete[] datas;
+		datas = NUll;
+	}
+
+	void Enqueue(T data)
+	{
+		int position = 0;
+		if (rear == capacity)
+		{
+			position = rear;
+			rear = 0;
+		}
+		else
+			position = rear++;
+
+		datas[position] = data;
+	}
+
+	T Deque()
+	{
+		int position = front;
+		if (front == capacity)
+		{
+			front = 0;
+		}
+		else
+		{
+			front++;
+		}
+		return datas[position];
+	}
+private:
+	T* datas;
+
+	int capacity;
+	int front = 0;//앞
+	int rear = 0;//뒤
+
+};
