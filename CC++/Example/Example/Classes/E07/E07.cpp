dc945dd1db1b2f765585f25b6d5705dc9d96f840
KitchenGun <41855240+KitchenGun@users.noreply.github.com>
2021-03-29 21:54:53 +0900

    6일차 함수 선언 및 구현

--- CC++/Example/Example/Classes/E07/E07.cpp
+++ CC++/Example/Example/Classes/E07/E07.cpp
@@ -1,6 +1,78 @@
 #include "E07.h"
+//#include는 헤더 파일 내용을 전부 복사
+//#define P07_FUNC_A		1
+#define P07_FUNC_B		2
+#define P07_FUNC_C		3
+
+
+#if P07_FUNC_A
+//! 덧셈 결과를 반환한다
+int P07GetSumValue(int a_nLhs, int a_nRhs)//a = 전달값이라는 의미 nLhs 좌항  nRhs 우항
+{
+	/*
+	return 키워드는 함수의 흐름을 종료하고 해당함수를 호출한 곳으로 되돌아가는 역할을 수행한다 또한 해당 함수가 반환 값이 존재 할 경우 해당 키워드에 
+	명시된 결과 값을 호출한 곳으로 반환한다.
+	void 일 경우 
+	return;
+	*/
+	return a_nLhs + a_nRhs;
+}
+//! 뺄셈 결과를 반환한다
+int P07GetSubValue(int a_nLhs, int a_nRhs)
+{
+	return a_nLhs - a_nRhs;
+}
+//! 곱셈 결과를 반환한다
+int P07GetMultiplyValue(int a_nLhs, int a_nRhs)
+{
+	return a_nLhs * a_nRhs;
+}
+//! 나누셈 결과를 반환한다
+float P07GetDivideValue(int a_nLhs, int a_nRhs)
+{
+	return a_nLhs / (float)a_nRhs;
+}
+#elif P07_FUNC_B
+
+#endif //#if P07_FUNC_A
 
 void E07(int argc, char ** args)
 {
+#if P07_FUNC_A
+	int nValueA = 0;
+	int nValueB = 0;
+
+	char chOperator = '\0';//null 문자 문자형 변수 초기화용
+
+	printf("수식입력 + - * /) : ");
+	scanf("%d %c %d", &nValueA, &chOperator, &nValueB);
+
+	if (chOperator == '+')
+	{
+		int nResult = P07GetSumValue(nValueA, nValueB);
+		printf("%d + %d = %d\n", nValueA, nValueB, nResult);
+	}
+	else if (chOperator == '-')
+	{
+		int nResult = P07GetSubValue(nValueA, nValueB);
+		printf("%d - %d = %d\n", nValueA, nValueB, nResult);
+	}
+	else if (chOperator == '*')
+	{
+		int nResult = P07GetMultiplyValue(nValueA, nValueB);
+		printf("%d * %d = %d\n", nValueA, nValueB, nResult);
+	}
+	else if (chOperator == '/')
+	{
+		float fResult = P07GetDivideValue(nValueA, nValueB);
+		printf("%d / %d = %f\n", nValueA, nValueB, fResult);
+	}
+	else
+	{
+		printf("잘못된 수식을 입력하였습니다.");
+	}
+#elif P07_FUNC_B
+#elif P07_FUNC_C
+#endif // P07_FUNC_A
 
 }
--- CC++/Example/Example/Classes/E07/E07.h
+++ CC++/Example/Example/Classes/E07/E07.h
@@ -2,5 +2,40 @@
 
 #include "../Global/Define/KGDefine.h"
 
+/*
+ 함수란 프로그램의 전체 또는 일부분을 그룹화 시켜서 따로 분리 시킬수있는 기능을 의미한다
+ 이렇게 따로 그룹화 된 코드는 필요할때 불러와서 재사용 하는 것이 가능하다
+ 즉 함수를 활용하면 프로그램 로직을 반복적으로 작성필요가 사라진다.
 
+c언어 함수 선언법
+반환형 함수이름 (매개변수1,매개변수2,매개변수3,...)
+
+ex)
+int SumFuncA(int a_nLhs, int a_nRhs);	input o output o
+void SumFuncA(int a_nLhs, int a_nRhs);  input o output x
+int SomeFuncC(void);					input x output o
+void SomeFuncD(void);					input x output x
+
+c언어 함수 구현법
+int SumFuncA(int a_nLhs, int a_nRhs){}
+void SumFuncA(int a_nLhs, int a_nRhs){}
+int SomeFuncC(void){}
+void SomeFuncD(void){}
+
+함수 선언 vs 함수 구현
+선언은 컴파일러에게 함수의 존재 사실을 알리는 역할  명시역할(중복가능)
+구현은 컴파일러에게 함수의 동작 방식을 알리는 역할	서술역할(중복불가능)
+
+공간(영역) 이란 ?
+C언어는 전역 공간과 지역 공간이라는 개념이 존재하며, 지역 공간은 {} (중괄호 연산자)를 사용해서 명시 가능하며
+특정 지역에 속해 있지 않는 모든 영역을 전역 공간이라고 한다
+함수는 선언 및 구현은 전역 공간에만 명시가능 지역 공간에는 주로 컴퓨터가 수행할 명령문들이 위치하게 된다.
+ c언어 변수 종류 
+ -지역 변수 : 특정 지역 내부에서만 사용가능 하고 해당 지역이 끝나면 메모리에서 사라진다
+ -전역 변수 : 전역 공간에 선언되는 변수 프로그램 전체에서 사용이 가능하다 프로그램 전체에 공유되는 변수를 의미 한다 프로그램 종료시 메모리에서 사라짐
+ -정적 지역 변수
+ -정적 전역 변수
+ */
 void E07(int argc, char **args);
+
+
