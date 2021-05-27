cf48b16b8bdfeb5dd29114dfee829e96f4b9828d
KitchenGun <kitchengun124@gmail.com>
2021-05-27 15:56:29 +0900

    DX에서 자원에 대한 접근방식 복습
    D3D11_USAGE_DEFAULT
    D3D11_USAGE_IMMUTABLE
    D3D11_USAGE_DYNAMIC
    D3D11_USAGE_STAGING
    복습 ref link(https://m.blog.naver.com/PostView.naver?blogId=masca140&logNo=220719096462&proxyReferer=https:%2F%2Fwww.google.com%2F)

--- DirectX2D/DXExample2017/DXExampleTriangle/DXExample/Program.cpp
+++ DirectX2D/DXExample2017/DXExampleTriangle/DXExample/Program.cpp
@@ -23,7 +23,7 @@ Program::Program()
 		D3D11_USAGE_DEFAULT	= gpu만 데이터 읽고 쓸수있다
 		D3D11_USAGE_IMMUTABLE = gpu가 데이터를 읽기만 한다/cpu접근 마찬가지로 불가능/생성과 동시에 초기화를 해야한다/제일 빠르다
 		D3D11_USAGE_DYNAMIC	= gpu 읽기 허용	/cpu 쓰기 허용  /맵 언맵을 사용해서 cpu에서 접근해서 수정이 가능함/cpu에서 한프레임당 한번씩 업데이트 필요한 자원에 사용
-		D3D11_USAGE_STAGING	= gpu 메모리에서 cpu 메모리로 복사 허용   /속도가 제일 느림
+		D3D11_USAGE_STAGING	= gpu 메모리에서 cpu 메모리로 복사 허용 (읽기 쓰기 전부 다됨)  /속도가 제일 느림
 		
 		*/
 	}
