d46acd4e538828d677f8b6a5de15311597c01c11
KitchenGun <kitchengun124@gmail.com>
2021-07-13 19:52:37 +0900

    Observer패턴 제작중
    참조 링크를 참고하여서  재 사용 가능한 코드를 작성하고자 함
    ref link(https://anywayjhwa.tistory.com/7)

--- DataStructure/ObserverPattern/ObserverPattern/ObserverPattern/Observer/Observer.h
+++ DataStructure/ObserverPattern/ObserverPattern/ObserverPattern/Observer/Observer.h
@@ -4,5 +4,7 @@ class Observer
 public: 
 	Observer() {}
 	virtual ~Observer() {} 
-	virtual void onNotify() = 0; 
+	virtual void onNotify() = 0; //등록된 객체들에게 전달
+	virtual void RegisterObserver() = 0; //수신받을 객체를 등록하는 함수
+	virtual void RemoveObserver() = 0;	//수신을 받던 객체의 수신을 해제하는 함수
 };
