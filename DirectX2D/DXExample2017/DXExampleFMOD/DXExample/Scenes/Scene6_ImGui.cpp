c01322feccc277f863eb0500623a1141ef9b0f67
KitchenGun <kitchengun124@gmail.com>
2021-07-13 18:49:49 +0900

    Path의 CreateFolders 함수 filter 기능 복습

--- DirectX2D/DXExample2017/DXExampleFMOD/DXExample/Scenes/Scene6_ImGui.cpp
+++ DirectX2D/DXExample2017/DXExampleFMOD/DXExample/Scenes/Scene6_ImGui.cpp
@@ -8,7 +8,8 @@ Scene6::Scene6()
 	Path::CreateFolders(L"./Directory/Sub1/Sub1_1");
 	Path::CreateFolders(L"./Directory/Sub1/Sub1_2");
 	Path::CreateFolders(L"./Directory/Sub2/Sub2_1");
-
+	//L"*" 모든 파일을 불러옴
+	//L"*.*" .을 가지고 있는 파일을 모두 불러옴
 	Path::GetFiles(&paths, L"./Directory/", L"*", true);
 }
 
