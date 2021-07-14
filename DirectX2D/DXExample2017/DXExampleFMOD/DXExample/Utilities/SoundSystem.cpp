0e2f720090732f2a34b30112ad8642be44f82a59
KitchenGun <kitchengun124@gmail.com>
2021-07-14 14:40:08 +0900

    FMod 사용한 사운드 시스템 복습
    주석작성으로 구조 파악

--- DirectX2D/DXExample2017/DXExampleFMOD/DXExample/Utilities/SoundSystem.cpp
+++ DirectX2D/DXExample2017/DXExampleFMOD/DXExample/Utilities/SoundSystem.cpp
@@ -59,8 +59,65 @@ void SoundSystem::SetLoop(bool val)
 
 void SoundSystem::GUI(int ordinal)
 {
+<<<<<<< Updated upstream
+=======
+	string objName = "Sound" + to_string(ordinal);
+	string GUIFileName = "File Name : " + fileName;
+	if (ImGui::BeginMenu(objName.c_str()))
+	{
+		ImGui::Text(objName.c_str());
+		ImGui::Text(GUIFileName.c_str());
+		//음원 교체
+		if (ImGui::Button("ChangeSound", ImVec2(100, 30)))
+		{
+			ChangeSoundFunc();
+			if (bPlaying)
+			{
+				Play();
+			}
+		}
+		//볼륨 조절
+		ImGui::SliderFloat("Volume", &volume, 0.0f, 1.0f, "%.2f");
+		SetVolume(volume);
+		//뮤트
+		ImGui::Checkbox("Mute", &bMute);
+		SetMute(bMute);
+
+		ImGui::EndMenu();
+	}
+>>>>>>> Stashed changes
 }
 
 void SoundSystem::ChangeSoundFunc(const wstring & path)
 {
+<<<<<<< Updated upstream
+=======
+	if (path.length() < 1)
+	{
+		//경로가 없을 경우
+		function<void(wstring)> func = bind(&SoundSystem::ChangeSoundFunc, this, placeholders::_1);
+		Path::OpenFileDialog(L"", Path::SoundFilter, L"./_Sounds/", func, handle);
+	}
+	else
+	{
+		size_t index = fileName.find_last_of('(');
+
+		fileName = Path::GetFileName(String::ToString(path)) + " " + fileName.substr(index, fileName.length());
+		if (bPlaying)
+		{
+			Stop();
+			bPlaying = true;
+		}
+
+		if (bLoop)
+		{
+			CreateSound(String::ToString(path));
+			bLoop = true;
+		}
+		else
+		{
+			CreateSound(String::ToString(path));
+		}
+	}
+>>>>>>> Stashed changes
 }
