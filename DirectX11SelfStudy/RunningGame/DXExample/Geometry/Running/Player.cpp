41962cbdf893fe1249fc3e484b5cefb219d79730
KitchenGun <kitchengun124@gmail.com>
2021-07-16 15:49:26 +0900

    17주차 과제 - 1
    Running게임 제작
    1.player 점프 구현 (완성)+낙하속도 등속운동하는 문제 수정
    2.달려오는 적 충돌 시 판정
    3.점수
    4.사운드

--- DirectX11SelfStudy/RunningGame/DXExample/Geometry/Running/Player.cpp
+++ DirectX11SelfStudy/RunningGame/DXExample/Geometry/Running/Player.cpp
@@ -14,44 +14,40 @@ Player::~Player()
 
 void Player::Jump()
 {
-	static float fJumpFrame=0;
+	static float fJumpFrame = 0;
 	if (fJumpPower == 0)
 	{
 		fJumpFrame = 0;
 	}
-
+	
 	//상승 상황 점프
 	if (fJumpPower <= fMaxJumpSpeed)
 	{
-		if (fJumpPower >= 0)
-		{
-			fJumpPower = fMaxJumpSpeed - Math::Lerpf(0, fMaxJumpSpeed, fJumpFrame);
-			fJumpFrame += 0.01f;
-			Move({ 0,fJumpPower,0 });
-			MoveLocalRect(Vector4(0, fJumpPower, 0, fJumpPower));
-		}
-		else
-		{
-			fJumpFrame = 0;
-			fJumpPower = 0;
-			isJump = false;
-		}
+		fJumpPower = fMaxJumpSpeed - Math::Lerpf(0, fMaxJumpSpeed, fJumpFrame);
+		fJumpFrame += 0.01f;
+		Move({ 0,fJumpPower,0 });
+		MoveLocalRect(Vector4(0, fJumpPower, 0, fJumpPower));
+	}
+	GroundIntersect = Math::GroundIntersect(this);
+	if (GroundIntersect == true)
+	{
+		SetPosition(Vector3(150, 50, 0));
+		fJumpPower = 0;
+		isJump = false;
 	}
 	
 }
 
 void Player::Update()
 {
-	GroundIntersect = Math::GroundIntersect(this);
-
-	if (!GroundIntersect)
+	if(GroundIntersect)
 	{
-		Move({ 0, -2, 0});
-		MoveLocalRect(Vector4(0, -2, 0, -2));
+		nJumpCount = 0;
 	}
 	else
 	{
-		nJumpCount = 0;
+		Move({ 0,GravatiyPower,0 });
+		MoveLocalRect(Vector4(0, GravatiyPower, 0, GravatiyPower));
 	}
 
 	if (Keyboard::Get()->Down(VK_SPACE)&&nJumpCount < nMaxJumpCount)
@@ -66,4 +62,14 @@ void Player::Update()
 	{
 		Jump();
 	}
+}
+
+void Player::SetPosition(Vector3 position)
+{
+	this->position = position;
+	D3DXMatrixTranslation(&T, this->position.x, this->position.y, this->position.z);
+
+	world = S * R * T;
+	WB->SetWorld(world);
+	LB->SetLocalRect({ 100,100,200,0 });
 }
--- DirectX11SelfStudy/RunningGame/DXExample/Geometry/Running/Player.h
+++ DirectX11SelfStudy/RunningGame/DXExample/Geometry/Running/Player.h
@@ -8,13 +8,16 @@ public:
 public:
 	void Jump();
 	void Update() override;
+	void SetPosition(Vector3 position);
+
 private:
 	//점프 수치
 	bool isJump = false;
 	float fJumpPower=0.0f;
-	const float fMaxJumpSpeed = 10.0f;
+	const float fMaxJumpSpeed = 20.0f;
 	int nJumpCount = 0;
 	const int nMaxJumpCount = 2;
 	//지상 판정
+	float GravatiyPower = -10.0f;
 	bool GroundIntersect = true;
 };
