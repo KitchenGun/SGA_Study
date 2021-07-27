28c37b94973b4844b1765c8adfedb2ea65d5906a
KitchenGun <kitchengun124@gmail.com>
2021-07-27 19:26:14 +0900

    록맨 구조 복습
    작성한 록맨 구조 나눈 이유 분석

--- DirectX2D/DXExample2017/DXExampleTileMap/DXExample/Characters/Rockman.cpp
+++ DirectX2D/DXExample2017/DXExampleTileMap/DXExample/Characters/Rockman.cpp
@@ -29,7 +29,7 @@ void Rockman::SetAnimation()
 	//텍스쳐 주소 입력하여서 텍스쳐 자원 제작
 	texture = new Texture2D(L"./_Textures/록맨.bmp");
 	//애니메이션 객체를 만드는데 좌우로 나눠서 제작
-										//이미지 갯수,시작 점	,끝점
+													//이미지 갯수,시작 점	,끝점
 	animClips.push_back(new AnimationClip(L"RunR", texture, 10, { 0, 0 }, { (float)texture->GetWidth(), (float)texture->GetHeight() / 2 }));
 	animClips.push_back(new AnimationClip(L"RunL", texture, 10, { 0, (float)texture->GetHeight() / 2 }, { (float)texture->GetWidth(), (float)texture->GetHeight() }, true));
 	animator = new Animator(animClips);
--- DirectX2D/DXExample2017/DXExampleTileMap/DXExample/Characters/Rockman.h
+++ DirectX2D/DXExample2017/DXExampleTileMap/DXExample/Characters/Rockman.h
@@ -5,8 +5,10 @@ public:
 	Rockman(Vector3 position, Vector3 size, float rotation);
 	~Rockman();
 public:
+	//추후에 다른 애니메이션 추가가 있을 수 있으니 override로 변경
 	void Move(Vector3 position) override;
 public:
+	//생성자에서 애니메이션 클립을 만들고 애니메이션을 추가하는 함수
 	void SetAnimation();
 
 private:
