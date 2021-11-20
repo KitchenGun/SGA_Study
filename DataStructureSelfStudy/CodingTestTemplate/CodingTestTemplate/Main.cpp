6c43ffd92066ba7c9628f07660648b9cb54dcb13
KitchenGun <kitchengun124@gmail.com>
2021-11-20 23:40:04 +0900

    프로그래머스 1단계 테스트
    연습용

--- DataStructureSelfStudy/CodingTestTemplate/CodingTestTemplate/Main.cpp
+++ DataStructureSelfStudy/CodingTestTemplate/CodingTestTemplate/Main.cpp
@@ -45,28 +45,81 @@ int main()
 	cout << result << endl;
 	*/
 
-	int n; 
-	vector<int> arr;
-	
-	cin >> n;
-	for (int i = 0; i < n; i++)
-	{
-		int x;
-		cin >> x;
-		arr.push_back(x);
-	}
-	sort(arr.begin(), arr.end());
+	//int n; 
+	//vector<int> arr;
+	//
+	//cin >> n;
+	//for (int i = 0; i < n; i++)
+	//{
+	//	int x;
+	//	cin >> x;
+	//	arr.push_back(x);
+	//}
+	//sort(arr.begin(), arr.end());
+	//
+	//int result = 0;
+	//int cnt = 0;
+	//for (int i =0;i<n;++i)
+	//{
+	//	cnt += 1;
+	//	if (cnt >= arr[i])
+	//	{
+	//		result += 1;
+	//		cnt = 0;
+	//	}
+	//}
+	//cout << result << endl;
+	/*
+	배열 arr가 주어집니다. 배열 arr의 각 원소는 숫자 0부터 9까지로 이루어져 있습니다. 이때, 배열 arr에서 연속적으로 나타나는 숫자는 하나만 남기고 전부 제거하려고 합니다. 단, 제거된 후 남은 수들을 반환할 때는 배열 arr의 원소들의 순서를 유지해야 합니다. 예를 들면,
 
-	int result = 0;
-	int cnt = 0;
-	for (int i =0;i<n;++i)
+arr = [1, 1, 3, 3, 0, 1, 1] 이면 [1, 3, 0, 1] 을 return 합니다.
+arr = [4, 4, 4, 3, 3] 이면 [4, 3] 을 return 합니다.
+배열 arr에서 연속적으로 나타나는 숫자는 제거하고 남은 수들을 return 하는 solution 함수를 완성해 주세요.
+
+제한사항
+배열 arr의 크기 : 1,000,000 이하의 자연수
+배열 arr의 원소의 크기 : 0보다 크거나 같고 9보다 작거나 같은 정수
+	*/
+	//vector<int> arr = {1,1,3,3,0,1,1};
+	//vector<int> answer;
+	//answer.push_back(arr[0]);
+	//for (int i = 0; i < arr.size(); ++i)
+	//{
+	//	if (arr[i] != answer[answer.size() - 1])
+	//	{
+	//		answer.push_back(arr[i]);
+	//	}
+	//}
+	//
+	//
+	//cout << "Hello Cpp" << endl;
+	//
+	//
+	//for (int temp : answer)
+	//{
+	//	cout << temp;
+	//}
+	//문자열 정렬 소문자 -> 대문자로
+	/*
+	string s = "Zbcdefg";
+	for (int i = 0; i < s.size(); ++i)
 	{
-		cnt += 1;
-		if (cnt >= arr[i])
+		for (int j = 0; j < s.size(); ++j)
 		{
-			result += 1;
-			cnt = 0;
+			if (s[i] != s[j])
+			{
+				if (s[i] > s[j])
+				{
+					char temp = s[i];
+					s[i] = s[j];
+					s[j] = temp;
+				}
+			}
 		}
 	}
-	cout << result << endl;
+
+	string answer = s;
+
+	cout << s << endl;
+	*/
 }
