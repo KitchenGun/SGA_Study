a19fe9e05321c8dce7149f564a06c770a2aa0c77
KitchenGun <kitchengun124@gmail.com>
2021-11-21 21:19:00 +0900

    정렬 알고리즘 공부
    정렬 알고리즘 예제와
    프로그래머스 2단계 도전

--- DataStructureSelfStudy/CodingTestTemplate/CodingTestTemplate/Main.cpp
+++ DataStructureSelfStudy/CodingTestTemplate/CodingTestTemplate/Main.cpp
@@ -122,4 +122,113 @@ arr = [4, 4, 4, 3, 3] 이면 [4, 3] 을 return 합니다.
 
 	cout << s << endl;
 	*/
+
+
+	//정렬
+	//int n = 10;
+	//int target[10] = { 7,5,9,0,3,1,6,2,8,4 };
+	//배열에서 가장 작은 수를 찾아서 순서대로 정렬하는 방식 중복이 없어야함
+//for (int i = 0; i < (sizeof(target) / sizeof(int)); ++i)
+//{
+//	int min_index = i;
+//	for (int j = i + 1; j < (sizeof(target) / sizeof(int)); ++j)
+//	{
+//		if (target[min_index] > target[j])
+//		{
+//			min_index = j;
+//		}
+//	}
+//	swap(target[i], target[min_index]);
+//}
+
+	/*데이터 교체
+	for (int i = 1; i < 10; ++i)
+	{
+		for (int j = i; j > 0; --j)
+		{
+			if (target[j] < target[j-1])
+			{
+				swap(target[j], target[j-1]);
+			}
+			else
+				break;
+		}
+	}
+	*/
+	//계수 정렬
+	//int n = 15;
+	//int target[15] = { 7,5,9,0,3,1,6,2,9,1,4,8,0,5,2 };
+	//int maxValue = 9;
+	//int cnt[10] = {};
+	//
+	//for (int i = 0; i < n; ++i)
+	//{
+	//	cnt[target[i]] += 1;
+	//}
+	//
+	//for (int i = 0; i <= maxValue; ++i)
+	//{
+	//	for (int j = 0; j < cnt[i]; ++j)
+	//	{
+	//		cout << i ;
+	//	}
+	//}
+
+	//for (int temp : target)
+	//{
+	//	cout << temp;
+	//}
+
+	/*
+문자열 s에는 공백으로 구분된 숫자들이 저장되어 있습니다. str에 나타나는 숫자 중 최소값과 최대값을 찾아 이를 "(최소값) (최대값)"형태의 문자열을 반환하는 함수, solution을 완성하세요.
+예를들어 s가 "1 2 3 4"라면 "1 4"를 리턴하고, "-1 -2 -3 -4"라면 "-4 -1"을 리턴하면 됩니다.
+
+제한 조건
+s에는 둘 이상의 정수가 공백으로 구분되어 있습니다.
+입출력 예
+s	return
+"1 2 3 4"	"1 4"
+"-1 -2 -3 -4"	"-4 -1"
+"-1 -1"	"-1 -1"
+string solution(string s) {
+	string answer = "";
+	return answer;
+}
+*/
+	string s = "-11 0 100";
+
+	string answer = "";
+	int min, max;
+	int current = 0;
+	bool isPlus = true;
+	bool isSpace = true;
+	int SpaceCount = 0;
+
+	
+
+	/*
+	n개의 음이 아닌 정수가 있습니다. 이 수를 적절히 더하거나 빼서 타겟 넘버를 만들려고 합니다. 예를 들어 [1, 1, 1, 1, 1]로 숫자 3을 만들려면 다음 다섯 방법을 쓸 수 있습니다.
+
+-1+1+1+1+1 = 3
++1-1+1+1+1 = 3
++1+1-1+1+1 = 3
++1+1+1-1+1 = 3
++1+1+1+1-1 = 3
+사용할 수 있는 숫자가 담긴 배열 numbers, 타겟 넘버 target이 매개변수로 주어질 때 숫자를 적절히 더하고 빼서 타겟 넘버를 만드는 방법의 수를 return 하도록 solution 함수를 작성해주세요.
+
+제한사항
+주어지는 숫자의 개수는 2개 이상 20개 이하입니다.
+각 숫자는 1 이상 50 이하인 자연수입니다.
+타겟 넘버는 1 이상 1000 이하인 자연수입니다.
+입출력 예
+numbers	target	return
+[1, 1, 1, 1, 1]	3	5
+입출력 예 설명
+문제에 나온 예와 같습니다.
+
+int solution(vector<int> numbers, int target) {
+	int answer = 0;
+	return answer;
+}
+	*/
 }
