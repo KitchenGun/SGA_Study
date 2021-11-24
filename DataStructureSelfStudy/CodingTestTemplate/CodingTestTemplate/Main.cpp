a077d0b015affcd40e319a618fcda0ba9985c00b
KitchenGun <kitchengun124@gmail.com>
2021-11-24 17:09:41 +0900

    스택과 큐 공부
    스택과 큐 예제를 만들어봄

--- DataStructureSelfStudy/CodingTestTemplate/CodingTestTemplate/Main.cpp
+++ DataStructureSelfStudy/CodingTestTemplate/CodingTestTemplate/Main.cpp
@@ -1,6 +1,11 @@
 #include <iostream>
 #include <vector>
+#include <string>
 #include <algorithm>
+
+
+#include <stack>
+#include <queue>
 using namespace std;
 
 int main()
@@ -195,40 +200,53 @@ string solution(string s) {
 	return answer;
 }
 */
-	string s = "-11 0 100";
+	//string s = "-1 200 -100";
+//vector<int> vecInt;
+//string sTemp="";
+//string answer = "";
+//
+//
+//for (int i =0; i<s.size();++i)
+//{
+//	if (s[i] == ' ')
+//	{
+//		vecInt.push_back(stoi(sTemp));
+//		sTemp.clear();
+//		continue;
+//	}
+//	sTemp += s[i];
+//}
+//vecInt.push_back(stoi(sTemp));
+//sort(vecInt.begin(),vecInt.end());
+//
+//answer += to_string(vecInt.front());
+//answer += ' ';
+//answer += to_string(vecInt.back());
+//
+//cout<< answer;
 
-	string answer = "";
-	int min, max;
-	int current = 0;
-	bool isPlus = true;
-	bool isSpace = true;
-	int SpaceCount = 0;
+	stack<int> stack1;
 
-	
+	stack1.push(1);
+	stack1.push(2);
+	stack1.push(3);
 
-	/*
-	n개의 음이 아닌 정수가 있습니다. 이 수를 적절히 더하거나 빼서 타겟 넘버를 만들려고 합니다. 예를 들어 [1, 1, 1, 1, 1]로 숫자 3을 만들려면 다음 다섯 방법을 쓸 수 있습니다.
+	while (!stack1.empty())
+	{
+		cout << stack1.top() << endl;
+		stack1.pop();
+	}
 
--1+1+1+1+1 = 3
-+1-1+1+1+1 = 3
-+1+1-1+1+1 = 3
-+1+1+1-1+1 = 3
-+1+1+1+1-1 = 3
-사용할 수 있는 숫자가 담긴 배열 numbers, 타겟 넘버 target이 매개변수로 주어질 때 숫자를 적절히 더하고 빼서 타겟 넘버를 만드는 방법의 수를 return 하도록 solution 함수를 작성해주세요.
 
-제한사항
-주어지는 숫자의 개수는 2개 이상 20개 이하입니다.
-각 숫자는 1 이상 50 이하인 자연수입니다.
-타겟 넘버는 1 이상 1000 이하인 자연수입니다.
-입출력 예
-numbers	target	return
-[1, 1, 1, 1, 1]	3	5
-입출력 예 설명
-문제에 나온 예와 같습니다.
+	queue<int> queue1;
 
-int solution(vector<int> numbers, int target) {
-	int answer = 0;
-	return answer;
-}
-	*/
+	queue1.push(1);
+	queue1.push(2);
+	queue1.push(3);
+
+	while (!queue1.empty())
+	{
+		cout << queue1.front() << endl;
+		queue1.pop();
+	}
 }
