b3b448426581a2be02f8ada0a15eb77624635a2e
KitchenGun <kitchengun124@gmail.com>
2021-12-28 15:39:15 +0900

    bubble 정렬과 window QueryPerformanceFrequency 쓰는 법 공부

---
+++ DataStructureSelfStudy/Sort/Sort/BubbleSort.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <vector>
+#include "Timer.h"
+using namespace std;
+
+void BubbleSort(vector<int> &dataSet, int length)
+{
+	for (int i = 0; i < length; i++)
+	{
+		for (int j = 0; j < length - (i+1); j++)
+		{
+			if (dataSet[j] < dataSet[j + 1])
+			{
+				int temp = dataSet[j + 1];
+				dataSet[j + 1] = dataSet[j];
+				dataSet[j] = temp;
+			}
+		}
+
+		for (int temp : dataSet)
+		{
+			cout << temp;
+		}
+		cout << endl;
+	}
+}
+
+int main()
+{
+	Timer timer;
+	vector<int> nlist = { 1,4,3,2,5 };
+	for (int temp : nlist)
+	{
+		cout << temp;
+	}
+	cout << endl;
+	timer.Start();
+	BubbleSort(nlist, 5);
+	cout<<(float)timer.End()<<endl;
+	for (int temp : nlist)
+	{
+		cout << temp;
+	}
+}
---
+++ DataStructureSelfStudy/Sort/Sort/Timer.h
@@ -0,0 +1,26 @@
+#pragma once
+
+#include <Windows.h>
+
+class Timer
+{
+public:
+	Timer()
+	{
+		QueryPerformanceFrequency((LARGE_INTEGER*)(&tick));
+	}
+	void Start()
+	{
+		QueryPerformanceCounter((LARGE_INTEGER*)(&start));
+
+	}
+
+	float End()
+	{
+		QueryPerformanceCounter((LARGE_INTEGER*)(&end));
+		return (float)((end - start) / (tick / 1000.0f));
+	}
+private:
+	__int64 tick;
+	__int64 start, end;
+};
