b725077287802038b0819a5b2154d753cad4b6ae
KitchenGun <kitchengun124@gmail.com>
2022-05-02 14:59:09 +0900

    이진탐색 백준 문제 1920번
    이진 탐색으로 다시 풀어봄

--- DataStructureSelfStudy/CodingTestTemplate/CodingTestTemplate/1920.cpp
+++ DataStructureSelfStudy/CodingTestTemplate/CodingTestTemplate/1920.cpp
@@ -3,59 +3,73 @@
 #include <vector>
 using namespace std;
 
+vector<int> arr;
 
-
-using namespace std;
-
-int arr[100001];
+//틀린 이유 중복을 찾을려는 문제를 잘 읽지 않은 문제
+//멍청하게 무한 루프문을 만듬end = target;->end = target-1;이렇게 바꿔서 해결
 
 //이진탐색(Binary Search)을 이용하여 탐색
-void Solution(int n, int key) {
+int Solution(int n, int key) 
+{
+	int start = 0;
+	int end = n-1;
+	int target = (start+end)/2;
+	int count = 0;
 
-    int start = 0;
-    int end = n - 1;
-    int mid;
+	while (end>=start)
+	{
+		target = (start + end) / 2;
 
-    while (end - start >= 0) {
-        mid = (start + end) / 2;
+		if (arr[target]==key)
+		{
+			return 1;
+		}
+		else if (arr[target] > key)
+		{
+			end = target-1;
+		}
+		else
+		{
+			start = target+1;
+		}
+	}
+	return 0;
 
-        if (arr[mid] == key) {   //key 값이 배열의 중앙 값과 같을때
-            printf("1\n");
-            return;
-
-        }
-        else if (arr[mid] > key) { //key 값이 배열의 중앙 값보다 작을때 (왼쪽으로)
-            end = mid - 1;
-
-        }
-        else {  //key 값이 배열의 중앙 값보다 클때 (오른쪽으로)
-            start = mid + 1;
-        }
-    }
-
-    printf("0\n");
-    return;
 }
 
-int main(void) {
 
-    int n, m, tmp;
+int main(void) 
+{
+	int n,m;
 
-    //입력
-    cin>>n;
+	cin>>n;
 
-    for (int i = 0; i < n; i++) {
-        cin >> arr[i];
-    }
-    sort(arr, arr + n);   //quick sort를 이용해 배열 오름차순으로 정렬
+	for (int i = 0; i < n; i++)
+	{
+		int temp;
+		cin>>temp;
 
-    //입력
-   cin>>m;
+		arr.push_back(temp);
+	}
+   
+   //정렬
+   sort(arr.begin(),arr.end());
 
-    for (int i = 0; i < m; i++) {
-       cin>>tmp;
-        Solution(n, tmp);
-    }
+	cin>>m;
 
-    return 0;
+	vector<int> results;
+
+	for (int i = 0; i < m; i++)
+	{
+		int temp;
+		cin >> temp;
+
+		results.push_back(Solution(n,temp));
+	}
+
+	for (int i = 0; i < m; i++)
+	{
+		cout<<results[i]<<endl;
+	}
+
 }
