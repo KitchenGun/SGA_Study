a66db6c0e0efc8a34aa3e07738cbfbdc9c469dc5
KitchenGun <kitchengun124@gmail.com>
2022-06-29 17:13:49 +0900

    백준 dfs bfs 기초 문제
    1260 set을 사용하여 입력과 동시에 정렬을 하고 dfs bfs 방식에 맞게 탐색후 출력

--- DataStructureSelfStudy/CodingTestTemplate/CodingTestTemplate/1260.cpp
+++ DataStructureSelfStudy/CodingTestTemplate/CodingTestTemplate/1260.cpp
@@ -1,39 +1,70 @@
 #include <iostream>
-
+#include<queue>
+#include<set>
 #include <vector>
 
 using namespace std;
 
-void dfs(int start,vector<vector<int>>&arr,vector<bool>&visit)
+void dfs(int start,vector<set<int>>&arr,vector<bool>&visit)
 {
-	
+	visit[start] = true;
+	cout<<start+1<<" ";
+
+	for (set<int>::iterator it = arr[start].begin(); it != arr[start].end(); it++)
+	{
+		if (!visit[*it])
+		{
+			dfs(*it,arr,visit);
+		}
+	}
 }
 
-void bfs(int start, vector<vector<int>>& arr, vector<bool>& visit)
+void bfs(int start, vector<set<int>>& arr, vector<bool>& visit)
 {
+	queue<int> q;
+	q.push(start);
+	visit[start] = true;
 
+	while (!q.empty())
+	{
+		int here =q.front();
+		cout<<q.front()+1 << " ";
+		q.pop();
+
+		for (set<int>::iterator it = arr[here].begin();it != arr[here].end();it++)
+		{
+			if (!visit[*it])
+			{
+				q.push(*it);
+				visit[*it]=true;
+			}
+		}
+	}
 }
 
 
 int main()
 {
-	vector<vector<int>> arr;
+	vector<set<int>> arr;
 	vector<bool>visit;
 	int n,m,start;
 	cin>>n>>m>>start;
 
-	arr.assign(n,vector<int>(0,0));
+	arr.assign(n, set<int>{});
 	for (int i = 0; i < m; i++)
 	{
 		int a,b;
 		cin>>a>>b;
-		arr[a].push_back(b);
-		arr[b].push_back(a);
+		a--;
+		b--;
+		arr[a].insert(b);
+		arr[b].insert(a);
 	}
+
 	visit.assign(n,false);
-	dfs(start,arr,visit);
-
+	dfs(start-1,arr,visit);
 	cout<<endl;
-
 	visit.assign(n, false);
+	bfs(start-1,arr,visit);
+
 }
