9ed1b42c3b88b737f21160d87d758f09fdf66bbf
KitchenGun <kitchengun124@gmail.com>
2022-07-12 22:27:26 +0900

    bfs dp 문제 풀기

---
+++ DataStructureSelfStudy/CodingTestTemplate/CodingTestTemplate/2001.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main()
+{
+	for (int i = 0; i < 3; i++)
+	{
+		int temp;
+		cin>>temp;
+		
+
+	}
+
+}
--- DataStructureSelfStudy/CodingTestTemplate/CodingTestTemplate/2252.cpp
+++ DataStructureSelfStudy/CodingTestTemplate/CodingTestTemplate/2252.cpp
@@ -1,7 +1,34 @@
 #include <iostream>
 #include <vector>
-
+#include <queue>
 using namespace std;
+
+void solution(vector<vector<int>>& graph,vector<int> &visit)
+{
+	queue<int> q;
+
+	for (int i = 1; i < graph.size(); i++)
+	{
+		if(visit[i]==0)
+			q.push(i);
+	}
+	while (!q.empty())
+	{
+		int cur = q.front();
+		q.pop();
+		cout<<cur<<" ";
+		for (int i = 0; i < graph[cur].size(); i++)
+		{
+			int Next = graph[cur][i];
+			visit[Next]--;
+
+			if(visit[Next]==0)
+				q.push(Next);
+		}
+	}
+	cout<<endl;
+}
+
 
 int main()
 {
@@ -9,14 +36,16 @@ int main()
 
 	cin>>n>>m;
 
-	vector<vector<int>> graph(n,vector<int>(m,-1));
-	vector<bool> visit;
+	vector<vector<int>> graph(n+1,vector<int>(0));
+	vector<int> visit(n+1,0);
 	for (int i = 0; i < m; i++)
 	{
 		int a,b;
 		cin>>a>>b;
+		visit[b]++;
 		graph[a].push_back(b);
 	}
 
+	solution(graph,visit);
 
 }
