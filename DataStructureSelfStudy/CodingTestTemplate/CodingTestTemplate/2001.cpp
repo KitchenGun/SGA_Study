65291149eb69bd16d37314e3567ff152a6063c08
KitchenGun <kitchengun124@gmail.com>
2022-07-13 22:18:22 +0900

    그리디 알고리즘 문제 풀기

--- DataStructureSelfStudy/CodingTestTemplate/CodingTestTemplate/2001.cpp
+++ DataStructureSelfStudy/CodingTestTemplate/CodingTestTemplate/2001.cpp
@@ -4,12 +4,24 @@ using namespace std;
 
 int main()
 {
+	int a=INT32_MAX,b = INT32_MAX;
 	for (int i = 0; i < 3; i++)
 	{
 		int temp;
 		cin>>temp;
-		
+		if(a>temp)
+			a = temp;
 
 	}
-
+	for (int i = 0; i < 2; i++)
+	{
+		int temp;
+		cin >> temp;
+		if (b > temp)
+			b = temp;
+	}
+	float result = (a + b) * 1.1f;
+	cout<<fixed;
+	cout.precision(1);
+	cout<<result;
 }
---
+++ DataStructureSelfStudy/CodingTestTemplate/CodingTestTemplate/3120.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+
+using namespace std;
+
+void dfs(vector<int>&dgree,int start,int end)
+{
+	que
+}
+
+int main()
+{
+	int n,m;
+	cin>>n>>m;
+	vector<int> dgree(41,-1);
+
+	dfs(dgree,n,m);
+	cout<<dgree[m]
+}
