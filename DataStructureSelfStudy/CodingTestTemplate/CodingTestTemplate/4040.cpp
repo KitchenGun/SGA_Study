1bfbbafaee1176d32755922adcc6303d8ec25ddc
KitchenGun <kitchengun124@gmail.com>
2022-07-15 20:06:56 +0900

    그리디 알고리즘 문제 푸는중

--- DataStructureSelfStudy/CodingTestTemplate/CodingTestTemplate/4040.cpp
+++ DataStructureSelfStudy/CodingTestTemplate/CodingTestTemplate/4040.cpp
@@ -2,20 +2,66 @@
 #include <vector>
 using namespace std;
 
+int solution(vector<vector<char>> &map,int start,int end)
+{
+	vector<int> room;
+	for (int i = 0; i < map[start].size(); i++)
+	{
+		if (map[start][i] == 'O')//empty
+		{
+			room.push_back(i);
+		}
+	}
+	int result=0;
+
+	while (start < end)
+	{
+		int index =0;
+		for (int j = 1; j <= end; j++)
+		{
+			if (map[start][j] == 'O')
+			{
+				for (int k = start + 1; k <= end; k++)
+				{
+					if (!map[k][j])
+					{
+						index = max(index,k-1);
+						break;
+					}
+
+					if (k == end)
+					{
+						index =end;
+					}
+				}
+			}
+		}
+		if (index == 0)
+		{
+			return -1;
+		}
+		start = index+1;
+		result++;
+	}
+	return result;
+	
+}
+
 int main()
 {
 	int n,m;
 	cin>>n>>m;
-	vector<vector<char>> map(n,vector<char>(m));
+	vector<vector<char>> map(n,vector<char>(m+1));
 	for (int i = 0; i < n; i++)
 	{
-		for (int j = 0; j < m; j++)
+		for (int j = 1; j <= m; j++)
 		{
 			char temp;
 			cin>>temp;
 			map[i][j] = temp;
 		}
 	}
-
-
+	int start,end;
+	cin>>start>>end;
+	cout<<solution(map,start,end);
 }
