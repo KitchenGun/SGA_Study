d2857b97720cca109d8d1efcaed65ef2bbcf3533
KitchenGun <kitchengun124@gmail.com>
2022-07-16 23:50:09 +0900

    그리디 문제 수정
    더 깔끔한 방식으로 변경

--- DataStructureSelfStudy/CodingTestTemplate/CodingTestTemplate/4040.cpp
+++ DataStructureSelfStudy/CodingTestTemplate/CodingTestTemplate/4040.cpp
@@ -1,67 +1,51 @@
-#include <iostream>
-#include <vector>
+#include <iostream> 
+
 using namespace std;
 
-int solution(vector<vector<char>> &map,int start,int end)
-{
-	vector<int> room;
-	for (int i = 0; i < map[start].size(); i++)
-	{
-		if (map[start][i] == 'O')//empty
-		{
-			room.push_back(i);
-		}
-	}
-	int result=0;
+int n, m;
+int day_begin, day_end;
+char arr[101][31];
 
-	while (start < end)
-	{
-		int index =0;
-		for (int j = 1; j <= end; j++)
-		{
-			if (map[start][j] == 'O')
-			{
-				for (int k = start + 1; k <= end; k++)
-				{
-					if (!map[k][j])
-					{
-						index = max(index,k-1);
-						break;
-					}
+//가장 오래 머물 수 있는 방 찾기 
+int greedy(int index) {
 
-					if (k == end)
-					{
-						index =end;
-					}
-				}
-			}
+	int max = 0;
+	for (int i = 0; i < m; i++) {	//1번방부터 m번방까지
+		int day = 0;
+		for (int j = index; j < day_end - 1; j++) {	//index일부터 
+			if (arr[j][i] == 'O') day++;	//머물수 있다면 다음날 해당방 확인(루프계속) 
+			else break;
 		}
-		if (index == 0)
-		{
-			return -1;
-		}
-		start = index+1;
-		result++;
+		if (max < day) max = day;
 	}
-	return result;
-	
+	return max;
 }
 
-int main()
-{
-	int n,m;
+int main() {
+
 	cin>>n>>m;
-	vector<vector<char>> map(n,vector<char>(m+1));
-	for (int i = 0; i < n; i++)
-	{
-		for (int j = 1; j <= m; j++)
-		{
-			char temp;
-			cin>>temp;
-			map[i][j] = temp;
+
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < m; j++) {
+			cin >> arr[i][j];
 		}
 	}
-	int start,end;
-	cin>>start>>end;
-	cout<<solution(map,start,end);
+
+	//s와 t입력
+	cin >> day_begin >> day_end;
+
+	int cnt = -1;	//옮긴 횟수 
+	//s일부터 t일까지
+	for (int i = day_begin - 1; i < day_end - 1;) {
+		cnt++;
+		//예약이 불가능한 경우
+		if (greedy(i) == 0) {
+			cnt = -1;
+			break;
+		}
+		//날짜에 대하여 가장 오래 머물수 있는 방 찾기 
+		i += greedy(i);	//그 기간만큼 더해주기
+	}
+
+	printf("%d", cnt);
 }
