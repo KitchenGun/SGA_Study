34b42ff9451e33d2f0d411aa0c64570fda00dbbf
KitchenGun <kitchengun124@gmail.com>
2022-07-17 22:17:09 +0900

    그리디 문제 완벽하게 이해
    배열을 탐색할때 arr[i][j]가 아닌 arr[j][i] 방식으로 해결하면 된다

--- DataStructureSelfStudy/CodingTestTemplate/CodingTestTemplate/4040.cpp
+++ DataStructureSelfStudy/CodingTestTemplate/CodingTestTemplate/4040.cpp
@@ -1,51 +1,58 @@
 #include <iostream> 
-
+#include <vector>
 using namespace std;
 
-int n, m;
-int day_begin, day_end;
-char arr[101][31];
+
 
 //가장 오래 머물 수 있는 방 찾기 
-int greedy(int index) {
-
+int greedy(vector<vector<char>>&arr,int start,int end)
+{
 	int max = 0;
-	for (int i = 0; i < m; i++) {	//1번방부터 m번방까지
+	for (int i = 0; i < arr[0].size(); i++) 
+	{	//1번방부터 m번방까지
 		int day = 0;
-		for (int j = index; j < day_end - 1; j++) {	//index일부터 
-			if (arr[j][i] == 'O') day++;	//머물수 있다면 다음날 해당방 확인(루프계속) 
-			else break;
+		for (int j = start; j < end; j++)
+		{	//index일부터 
+			if (arr[j][i] == 'O') 
+				day++;	//머물수 있다면 다음날 해당방 확인(루프계속) 
+			else
+				break;
 		}
-		if (max < day) max = day;
+
+		if (max < day) 
+			max = day;
 	}
 	return max;
 }
 
-int main() {
-
+int main()
+{
+	int n,m;
 	cin>>n>>m;
-
-	for (int i = 0; i < n; i++) {
-		for (int j = 0; j < m; j++) {
-			cin >> arr[i][j];
+	vector<vector<char>> arr(n,vector<char>(m));
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < m; j++)
+		{
+			cin>>arr[i][j];
 		}
 	}
 
-	//s와 t입력
-	cin >> day_begin >> day_end;
+	int start,end;
+	cin>>start>>end;
 
-	int cnt = -1;	//옮긴 횟수 
-	//s일부터 t일까지
-	for (int i = day_begin - 1; i < day_end - 1;) {
-		cnt++;
-		//예약이 불가능한 경우
-		if (greedy(i) == 0) {
-			cnt = -1;
-			break;
+	int count =-1;
+	for (int i = start - 1; i < end - 1;)
+	{
+		count++;
+		int curCount = greedy(arr, i, end - 1);
+		if (curCount ==0)
+		{
+			count = -1;
+			break;		
 		}
-		//날짜에 대하여 가장 오래 머물수 있는 방 찾기 
-		i += greedy(i);	//그 기간만큼 더해주기
+		i+= curCount;
 	}
-
-	printf("%d", cnt);
+	
+	cout<<count;
 }
