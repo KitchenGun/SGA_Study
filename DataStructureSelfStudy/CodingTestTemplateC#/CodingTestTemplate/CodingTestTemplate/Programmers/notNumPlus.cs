ff58841647a7e3ef596c067fea20f7aac798bf11
KitchenGun <kitchengun124@gmail.com>
2022-07-28 21:08:08 +0900

    C# 복습 프로그래머스 없는 숫자 더하기

---
+++ DataStructureSelfStudy/CodingTestTemplateC#/CodingTestTemplate/CodingTestTemplate/Programmers/notNumPlus.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Programmers.notNumPlus
+{
+    public class notNumPlus
+    {
+        public int solution(int[] numbers)
+        {
+            int answer = 0;
+            int[] arr = new int[10];
+
+            foreach(int temp in numbers)
+            {
+                arr[temp]++;
+            }
+
+
+            for (int i =0;i<10;i++)
+            {
+                if(arr[i]==0)
+                {
+                    answer += i;
+                }
+            }
+
+            return answer;
+        }
+    }
+}
---
+++ DataStructureSelfStudy/CodingTestTemplateC#/CodingTestTemplate/CodingTestTemplate/program/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Programmers.notNumPlus;
+
+namespace CodingTestTemplate
+{
+
+    class Program
+    {
+        
+
+        static void Main(string[] args)
+        {
+            notNumPlus solClass = new notNumPlus();
+            int answer = solClass.solution(new int[] {1, 2, 3, 4, 6, 7, 8, 0});
+
+            Console.WriteLine(answer);
+        }
+    }
+}
