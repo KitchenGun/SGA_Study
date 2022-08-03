8a8c8b8689835b0584ce6becddc7680a9077bb4e
KitchenGun <kitchengun124@gmail.com>
2022-08-03 19:44:27 +0900

    프로그래머스 완전탐색 문제 풀기
    이중 for문 시간 초과를 해결하는 법을 찾지못해서 어려웠다.

---
+++ DataStructureSelfStudy/CodingTestTemplate/CodingTestTemplate/carpet.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <vector>
+
+
+using namespace std;
+
+vector<int> solution(int brown, int yellow) 
+{
+    vector<int> answer;
+    int sum  = brown+yellow;
+
+
+    for (int height = 3;; height++)
+    {
+        if (!(sum % height))
+        {
+            int weight = sum/height;
+            if (((height - 2) * (weight - 2)) == yellow) 
+            {
+                answer.push_back(weight);
+                answer.push_back(height);
+                break;
+            }
+        }
+    }
+
+    return answer;
+}
+
+
+int main()
+{
+    vector<int> a = solution(10,2);
+
+
+    
+}
