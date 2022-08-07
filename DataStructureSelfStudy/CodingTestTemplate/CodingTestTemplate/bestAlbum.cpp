dce62c7571578623ac540e32dcf3fd893f574094
KitchenGun <kitchengun124@gmail.com>
2022-08-07 17:54:16 +0900

    프로그래머스 해시 문제 풀기
    어제 고민하던 문제 풀었다. 방향은 맞았는데 한글 이슈가 있었다...

--- DataStructureSelfStudy/CodingTestTemplate/CodingTestTemplate/bestAlbum.cpp
+++ DataStructureSelfStudy/CodingTestTemplate/CodingTestTemplate/bestAlbum.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
 #include <string>
 #include <map>
 #include <set>
@@ -7,43 +8,135 @@
 using namespace std;
 
 
-vector<int> solution(vector<string> genres, vector<int> plays)
-{
-    vector<int> answer;
+//vector<int> solution(vector<string> genres, vector<int> plays) 혼자서 풀엇지만 틀렸음
+//{
+//    vector<int> answer;
+//
+//    map<int,pair<string,int>> info;
+//    map<string, set<int>> genresinfo;
+//    int maxindex = 0;
+//    for (int i = 0; i < genres.size(); i++)
+//    {
+//        pair<string,int> temp = { genres[i],i };
+//        info.insert({plays[i],temp});
+//        maxindex = plays[maxindex]<=plays[i]?i: maxindex;
+//        if (genresinfo.find(genres[i]) == genresinfo.end())//못찾은 경우
+//        {
+//            set<int> a;
+//            a.insert(plays[i]);
+//            genresinfo.insert({genres[i],a});
+//
+//        }
+//        else//찾은경우
+//        {
+//            genresinfo[genres[i]].insert(plays[i]);
+//        }
+//    }
+//
+//
+//    //가장 들은 횟수가 많은 것을 집어 넣음
+//    answer.push_back(info[plays[maxindex]].second);
+//
+//    //장르를 고름
+//    string gen = info[plays[maxindex]].first;
+//    info.erase(plays[maxindex]);
+//
+//    //해당장르 다음 음악을 선택
+//    if (genresinfo[gen].size() > 0)
+//    {
+//        set<int>::iterator iter = genresinfo[gen].end();
+//        int a = info[*--iter].second;
+//        answer.push_back(a);
+//        info.erase(a);
+//    }
+//
+//    genresinfo.erase(gen);
+//
+//    //다음 장르
+//    
+//    for (map<int, pair<string, int>>::iterator iter = --info.end(); iter != info.begin(); --iter)
+//    {
+//        pair<string, int> temp = (*iter).second;
+//        if (gen != temp.first)
+//        {
+//            gen = temp.first;
+//            break;
+//        }
+//    }
+//
+//    answer.push_back(info[*--genresinfo[gen].end()].second);
+//
+//    //해당 장르 다음 음악 선택
+//    if (genresinfo[gen].size() > 0)
+//    {
+//        set<int>::iterator iter = --(genresinfo[gen].end());
+//        int a = info[*iter].second;
+//        answer.push_back(a);
+//    }
+//
+//    return answer;
+//}
 
-    map<int,pair<string,int>> info;
-    map<string, set<int>> genresinfo;
-    vector<bool> check(genres.size(),false);
+
+
+vector<int> solution(vector<string> genres, vector<int> plays) {
+    vector<int> answer;
+    //각 장르별로 횟수저장
+    map<string, int> music;
+    //각 장르별로 무슨노래가 몇번씩 저장됬는지
+    map<string, map<int, int>> musiclist;
 
     for (int i = 0; i < genres.size(); i++)
     {
-        pair<string,int> temp = { genres[i],i };
-        info.insert({plays[i],temp});
-        if (genresinfo.find(genres[i]) == genresinfo.end())//못찾은 경우
+        music[genres[i]]+=plays[i];
+        musiclist[genres[i]][i] =plays[i];
+    }
+
+
+
+    while (!music.empty())
+    {
+        string gen;
+        int max =0;
+        for (auto temp : music)
         {
-            set<int> a;
-            a.insert(plays[i]);
-            genresinfo.insert({genres[i],a});
+            if (max < temp.second)
+            {
+                gen = temp.first;
+                max = temp.second;
+            }
 
         }
-        else//찾은경우
+        for (int i = 0; i < 2; i++)
         {
-            genresinfo[genres[i]].insert(plays[i]);
+            int val = 0, ind = -1;
+            //노래중에서 제일높은것 찾기
+            for (auto ml : musiclist[gen])
+            {
+                if (val < ml.second) 
+                {
+                    val = ml.second;
+                    ind = ml.first;
+                }
+            }
+            if(ind!=-1)
+            {
+                answer.push_back(ind);
+                musiclist[gen].erase(ind);
+            }
         }
+        music.erase(gen);
     }
-    //가장 들은 횟수가 많은 것을 집어 넣음
-    answer.push_back(info[genres.size()-1].second);
-    check[info[genres.size() - 1].second] = true;
-    //미해결
-    https://school.programmers.co.kr/learn/courses/30/lessons/42579?language=cpp
     return answer;
 }
+
+
 
 int main()
 {
     vector<int> answer;
 
-    answer = solution({"classic", "pop", "classic", "classic", "pop"},{500, 600, 150, 800, 2500});
+    answer = solution({"a","classic", "pop", "classic", "classic", "pop"},{3100,500,  600, 150, 800, 2500});
 
     for (auto temp : answer)
     {
