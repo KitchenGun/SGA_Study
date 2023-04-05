89f582e093760ec90dcdbc7ae733ee68b7644e21
KitchenGun <41855240+KitchenGun@users.noreply.github.com>
2023-04-05 17:29:17 +0900

    백준 동적 계획 문제 풀기
    1463

--- DataStructureSelfStudy/CodingTestTemplate/CodingTestTemplate/1463.cpp
+++ DataStructureSelfStudy/CodingTestTemplate/CodingTestTemplate/1463.cpp
@@ -6,19 +6,28 @@ using namespace std;
 
 int main()
 {
-	vector<int>dp(1000001,0);
+	
 
 	int n;
 
 	cin>>n;
-	
+	vector<int>dp(n+1, 0);
+	dp[1] = 0;
+
 	for (int i = 2; i <= n; i++)//2부터 시작
 	{
-		dp[i] = dp[i-1]+1;//가장 큰값을 넣음
-		if(i%2==0)
-			dp[i]=min(dp[i],dp[i/2]+1);//2로 나눠질 경우 작은 값을 넣음
-		else if(i%3 ==0)
-			dp[i]=min(dp[i],dp[i/3]+1);//3으로 나눠질경우 작은 값을 넣음
+		dp[i] = dp[i - 1] + 1;//가장 큰값을 넣음
+		if (i % 3 == 0)
+		{
+			dp[i] = min(dp[i], dp[i / 3] + 1);//3으로 나눠질경우 작은 값을 넣음
+		}
+		if (i % 2 == 0)
+		{
+			dp[i] = min(dp[i], dp[i / 2] + 1);//2로 나눠질 경우 작은 값을 넣음
+		}
+
 	}
-	cout<<dp[n];
+	cout << dp[n]<<endl;
+
+	return 0;
 }
