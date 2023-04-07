8ea7b71d0f3b28d704663680d69b7f3c36af4c85
KitchenGun <41855240+KitchenGun@users.noreply.github.com>
2023-04-07 20:59:39 +0900

    백준 동적 계획 문제 풀기
    11053

---
+++ DataStructureSelfStudy/CodingTestTemplate/CodingTestTemplate/11053.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+    int n, max_val = 0;
+    int dp[1001];
+    dp[1] = 1;
+
+    cin >> n;
+    vector<int>arr(n+1,0);
+    for (int i = 1; i <= n; i++) 
+    {
+        cin >> arr[i];
+        for (int j = 1; j < i; j++) 
+        {
+            if (arr[i] > arr[j]) {
+                max_val = max(max_val, dp[j]);
+            }
+        }
+        dp[i] = max_val + 1;
+        max_val = 0;
+    }
+    for (int i = 1; i <= n; i++) 
+    {
+        max_val = max(max_val, dp[i]);
+    }
+    cout << max_val << '\n';
+
+    return 0;
+}
